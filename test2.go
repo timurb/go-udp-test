37a80d03de6d63a8ee601a4b8a06d9404e5427e5
Timur Batyrshin <erthad@gmail.com>
2015-11-14 15:36:54 +0300

    use 127.0.0.1 instead of localhost

--- test2.go
+++ test2.go
@@ -8,7 +8,7 @@ import "strconv"
 const MAX = 1000000
 
 func main() {
-  conn, err := net.Dial("udp", "localhost:1234")
+  conn, err := net.Dial("udp", "127.0.0.1:1234")
   if err != nil {
     fmt.Printf("Error while connecting to socket\n")
     os.Exit(1)
